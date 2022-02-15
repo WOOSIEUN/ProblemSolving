c0e737b5fae62bf2526a43703d67161d6ada2680
WOOSIEUN <ramtk6726@naver.com>
2022-02-15 13:27:27 +0900

    JUNGOL 1828

--- ProblemSolving/JUNGOL_1828.java
+++ ProblemSolving/JUNGOL_1828.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class JUNGOL_1828 {
@@ -11,6 +13,28 @@ public class JUNGOL_1828 {
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 		int N = Integer.parseInt(br.readLine());
+		int [][] chemical = new int[N][2];
+		for (int i = 0; i < chemical.length; i++) {
+			st = new StringTokenizer(br.readLine());
+			chemical[i][0] = Integer.parseInt(st.nextToken());
+			chemical[i][1] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(chemical, new Comparator<int[]>() {
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				return Integer.compare(o1[1], o2[1]);
+			}
+		});
+		int max = chemical[0][1], refri = 1;
+		for (int i = 0; i < chemical.length; i++) {
+			if(chemical[i][0] > max) {
+				refri++;
+				max = chemical[i][1];
+			}
+		}
+		bw.write(""+refri);
+		br.close();
+		bw.close();
 	}
 		
 }
