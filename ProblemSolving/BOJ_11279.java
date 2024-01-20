b0e7c2d414b29b636e34125863d0e127d896ab63
WOOSIEUN <ramtk6726@naver.com>
2024-01-20 20:29:56 +0900

    BOJ 11297 (Silver II)

---
+++ ProblemSolving/BOJ_11279.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class BOJ_11279 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		
+		int N = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+		for (int i = 0; i < N; i++) {
+			int num = Integer.parseInt(br.readLine());
+			
+			if (num == 0) {
+				sb.append(queue.isEmpty() ? 0 : queue.poll()).append("\n");
+			} else {
+				queue.add(num);
+			}
+		}
+		
+		bw.write(sb.toString());
+		br.close();
+		bw.close();
+	}
+}
