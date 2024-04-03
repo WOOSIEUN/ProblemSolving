47e150af9a3378e2fe9426859eb58687e4ec724e
WOOSIEUN <ramtk6726@naver.com>
2024-04-03 22:53:25 +0900

    BOJ 15652 (Silver III)

---
+++ ProblemSolving/BOJ_15652.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_15652 {
+	
+	static int N, M;
+	static int[] numbers;
+	static boolean[] visited;
+	static StringBuilder sb;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		visited = new boolean [N];
+		numbers = new int [M];
+		sb = new StringBuilder();
+		
+		combination(0, 0);
+		
+		bw.write(sb.toString());
+		br.close();
+		bw.close();
+	}
+	
+	static void combination(int cnt, int start) {
+        if (cnt == M) {
+            for (int num : numbers) {
+            	sb.append(num).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i < N; i++) {
+        	numbers[cnt] = i + 1;
+            combination(cnt + 1, i);
+        }
+    }
+}
