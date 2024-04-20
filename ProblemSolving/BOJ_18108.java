7977082c08cf7cd7a3e589c279783504d6afbb57
WOOSIEUN <ramtk6726@naver.com>
2024-04-20 23:58:09 +0900

    BOJ 18108 (Bronze V)

---
+++ ProblemSolving/BOJ_18108.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_18108 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		bw.write(String.valueOf(Integer.parseInt(br.readLine()) - 543));
+		br.close();
+		bw.close();
+	}
+}
