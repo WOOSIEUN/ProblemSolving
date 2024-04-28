4ca07e4d15556b5ca3238d51770185bf68d07b0a
WOOSIEUN <ramtk6726@naver.com>
2024-04-28 23:40:07 +0900

    BOJ 1085 (Bronze III)

---
+++ ProblemSolving/BOJ_1085.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_1085 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		int w = Integer.parseInt(st.nextToken());
+		int h = Integer.parseInt(st.nextToken());
+		
+		int xMinDist = Math.min(x, w - x);
+		int yMinDist = Math.min(y, h - y);
+		
+		bw.write(String.valueOf(Math.min(xMinDist, yMinDist)));
+		br.close();
+		bw.close();
+	}
+}
