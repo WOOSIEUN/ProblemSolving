5ab95dd66967604908cca4bfbd8cb993579e03ae
WOOSIEUN <ramtk6726@naver.com>
2024-05-04 22:55:28 +0900

    BOJ 2884 (Bronze III)

---
+++ ProblemSolving/BOJ_1018.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_1018 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int H = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		M -= 45;
+		
+		if (M < 0) {
+			M += 60;
+			H -= 1;
+			
+			if (H < 0) {
+				H = 23;
+			}
+		}
+		
+		bw.write(H + " " + M);
+		br.close();
+		bw.close();
+	}
+}
