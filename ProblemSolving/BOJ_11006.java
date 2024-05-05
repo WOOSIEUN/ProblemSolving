0ea1447ec42a00dc17167ce04dd82bd490be38df
WOOSIEUN <ramtk6726@naver.com>
2024-05-05 22:20:11 +0900

    BOJ 11006 (Bronze III)

---
+++ ProblemSolving/BOJ_11006.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_11006 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int T = Integer.parseInt(br.readLine());
+		for (int t = 0; t < T; t++) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			sb.append((M * 2) - N).append(" ").append(N - M).append("\n");
+		}
+		
+		bw.write(sb.toString());
+		br.close();
+		bw.close();
+	}
+}
