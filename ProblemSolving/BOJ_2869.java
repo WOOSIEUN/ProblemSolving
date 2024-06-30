9ea669e1d74619bebe80280df196c89aaccbbef0
WOOSIEUN <ramtk6726@naver.com>
2024-06-30 21:43:57 +0900

    BOJ 2869 (Bronze I)

---
+++ ProblemSolving/BOJ_2869.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class BOJ_2869 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int A = Integer.parseInt(st.nextToken());
+		int B = Integer.parseInt(st.nextToken());
+		int V = Integer.parseInt(st.nextToken());
+		
+		bw.write(String.valueOf((V - B - 1) / (A - B) + 1));
+		br.close();
+		bw.close();
+	}
+}
