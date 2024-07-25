5a7963a55f0caa06666adbe727dbba4efcce872b
WOOSIEUN <ramtk6726@naver.com>
2024-07-25 23:56:52 +0900

    BOJ 15894 (Bronze III)

---
+++ ProblemSolving/BOJ_15894.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_15894 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		long N = Long.parseLong(br.readLine());
+		bw.write(String.valueOf(4 * N));
+		br.close();
+		bw.close();
+	}
+}
