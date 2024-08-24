01b0e827ed0722e45e545206ddeb3873ef894619
WOOSIEUN <ramtk6726@naver.com>
2024-08-24 22:01:43 +0900

    BOJ 15727 (Bronze V)

---
+++ ProblemSolving/BOJ_15727.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_15727 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
+		int N = Integer.parseInt(br.readLine());
+		bw.write(String.valueOf(N / 5 + (N % 5 == 0 ? 0 : 1)));
+		br.close();
+		bw.close();
+	}
+}
