732ac340f1b72e905bf93e73d34440687dea20e2
Sieun Woo <ramtk6726@gmail.com>
2024-09-24 13:22:32 +0900

    BOJ 10101 (Bronze IV)

--- ProblemSolving/BOJ_10101.java
+++ ProblemSolving/BOJ_10101.java
@@ -3,32 +3,33 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class BOJ_10101 {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int A = Integer.parseInt(br.readLine());
-		int B = Integer.parseInt(br.readLine());
-		int C = Integer.parseInt(br.readLine());
-		
-		bw.write(getSortOfTriangle(A, B, C));
-		br.close();
-		bw.close();
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-	private static String getSortOfTriangle(int A, int B, int C) {
-		if (A == B && B == C) {
-			return "Equilateral";
-		} else if (A + B + C == 180) {
-			if (A == B || B == C || C == A) {
-				return "Isosceles";
-			} else {
-				return "Scalene";
-			}
-		} else {
-			return "Error";
-		}
-	}
+        int A = Integer.parseInt(br.readLine());
+        int B = Integer.parseInt(br.readLine());
+        int C = Integer.parseInt(br.readLine());
+
+        bw.write(getSortOfTriangle(A, B, C));
+        br.close();
+        bw.close();
+    }
+
+    private static String getSortOfTriangle(int A, int B, int C) {
+        if (A + B + C != 180) {
+            return "Error";
+        } else if (A == B && B == C) {
+            return "Equilateral";
+        } else {
+            if (A == B || B == C || C == A) {
+                return "Isosceles";
+            } else {
+                return "Scalene";
+            }
+        }
+    }
 }
