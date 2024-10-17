05ba44b49e713b1cb42d47d6f600e0879721cfde
WOOSIEUN <ramtk6726@naver.com>
2024-10-18 03:59:08 +0900

    BOJ 1010 (Silver V)

--- ProblemSolving/BOJ_1010.java
+++ ProblemSolving/BOJ_1010.java
@@ -6,19 +6,23 @@ import java.io.OutputStreamWriter;
 import java.util.StringTokenizer;
 
 public class BOJ_1010 {
+	
+	static int[][] dp;
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st;
 
-		int T = Integer.parseInt(br.readLine());
+		int T = Integer.parseInt(br.readLine());		
+		dp = new int[30][30];
+		
 		StringBuilder sb = new StringBuilder();
 		for (int t = 0; t < T; t++) {
 			st = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(st.nextToken());
 			int M = Integer.parseInt(st.nextToken());
-			int[][] dp = new int[M + 1][N + 1];
-			sb.append(combi(M, N, dp)).append("\n");
+			sb.append(combi(M, N)).append("\n");
 		}
 		
 		bw.write(sb.toString());
@@ -26,7 +30,7 @@ public class BOJ_1010 {
 		bw.close();
 	}
 	
-	public static int combi(int N, int R, int[][] dp) {
+	public static int combi(int N, int R) {
 		 if (dp[N][R] > 0) {
 			 return dp[N][R];
 		 }
@@ -35,6 +39,6 @@ public class BOJ_1010 {
 			return dp[N][R] = 1;
 		 }
 		 
-		 return combi(N - 1, R - 1, dp) + combi(N - 1, R, dp);
+		 return dp[N][R] = combi(N - 1, R - 1) + combi(N - 1, R);
 	}
 }
