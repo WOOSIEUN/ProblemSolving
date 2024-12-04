560d553f08afe49eb75f38b1d3f523cb43b9b5cc
WOOSIEUN <ramtk6726@naver.com>
2024-12-04 23:52:21 +0900

    BOJ 13301 (Silver V)

---
+++ ProblemSolving/BOJ_13301.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class BOJ_13301 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int N = Integer.parseInt(br.readLine());
+		long[] dp = new long[N + 1];
+		
+		dp[1] = 1;
+		for (int i = 2; i <= N; i++) {
+			dp[i] = dp[i - 1] + dp[i - 2];
+		}
+		
+		bw.write(String.valueOf(dp[N] * 2 + (dp[N] + dp[N - 1]) * 2));
+		br.close();
+		bw.close();
+	}
+}
