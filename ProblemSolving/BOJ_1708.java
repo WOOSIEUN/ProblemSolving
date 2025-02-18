c61b3127ac7b27afdc581791760a61af1eab2316
Sieun Woo <ramtk6726@gmail.com>
2025-02-18 13:17:57 +0900

    BOJ 1708 (Platinum V)

---
+++ ProblemSolving/BOJ_1708.java
@@ -0,0 +1,101 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class BOJ_1708 {
+    static class Point {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        ArrayList<Point> points = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            points.add(new Point(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
+        }
+
+        bw.write(String.valueOf(grahamScan(points)));
+        br.close();
+        bw.close();
+    }
+
+    private static int grahamScan(ArrayList<Point> points) {
+        Point root = getRoot(points);
+
+        points.sort(new Comparator<Point>() {
+            @Override
+            public int compare(Point p1, Point p2) {
+                int result = ccw(root, p1, p2);
+
+                if (result > 0) {
+                    return -1;
+                } else if (result < 0) {
+                    return 1;
+                } else {
+                    long distanceP1 = dist(root, p1);
+                    long distanceP2 = dist(root, p2);
+                    if (distanceP1 > distanceP2) {
+                        return 1;
+                    }
+                }
+                return -1;
+            }
+        });
+
+        Stack<Point> stack = new Stack<>();
+        stack.add(root);
+
+        for (int i = 1; i < points.size(); i++) {
+            while (stack.size() > 1 && (ccw(stack.get(stack.size() - 2), stack.get(stack.size() - 1), points.get(i)) <= 0)) {
+                stack.pop();
+            }
+            stack.add(points.get(i));
+        }
+
+        return stack.size();
+    }
+
+    private static Point getRoot(ArrayList<Point> points) {
+        Point root = new Point(Integer.MAX_VALUE, Integer.MAX_VALUE);
+
+        for (int i = 0; i < points.size(); i++) {
+            if (points.get(i).y < root.y) {
+                root = points.get(i);
+            } else if (points.get(i).y == root.y) {
+                if (points.get(i).x < root.x) {
+                    root = points.get(i);
+                }
+            }
+        }
+
+        return root;
+    }
+
+    private static int ccw(Point p1, Point p2, Point p3) {
+        long res = (long) (p2.x - p1.x) * (p3.y - p1.y) - (long) (p3.x - p1.x) * (p2.y - p1.y);
+        if (res < 0) {
+            return -1;
+        } else if (res > 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+    private static long dist(Point p1, Point p2) {
+        return (long) (p2.x - p1.x) * (p2.x - p1.x) + (long) (p2.y - p1.y) * (p2.y - p1.y);
+    }
+}
