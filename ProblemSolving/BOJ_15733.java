567362e12a884de053d13c877c65d7199f155c6d
Sieun Woo <ramtk6726@gmail.com>
2025-03-09 18:26:26 +0900

    BOJ 15733 (Bronze V)

---
+++ ProblemSolving/BOJ_15733.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class BOJ_15733 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("I'm Sexy");
+        bw.close();
+    }
+}
