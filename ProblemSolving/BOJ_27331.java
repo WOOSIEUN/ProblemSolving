69d3717abe3f452dc96227fb8b0162ae02751585
SIEUN WOO <50469745+WOOSIEUN@users.noreply.github.com>
2025-03-23 21:34:13 +0900

    BOJ 27331 (Bronze V)

---
+++ ProblemSolving/BOJ_27331.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class BOJ_27331 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf(br.readLine()) + String.valueOf(br.readLine()));
+        br.close();
+        bw.close();
+    }
+}
