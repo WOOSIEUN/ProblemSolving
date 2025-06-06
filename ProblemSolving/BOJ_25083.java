8d25538a20bb42f86c78a320348cd57c5d834541
Sieun Woo <ramtk6726@gmail.com>
2025-06-06 20:05:39 +0900

    BOJ 25083 (Bronze V)

---
+++ ProblemSolving/BOJ_25083.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class BOJ_25083 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.valueOf("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |"));
+        bw.close();
+    }
+}
