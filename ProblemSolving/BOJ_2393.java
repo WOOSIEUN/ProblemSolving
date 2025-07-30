134f585465e8643be3009e9edb1650c1c4af1eb8
Sieun Woo <ramtk6726@gmail.com>
2025-07-30 13:35:37 +0900

    BOJ 2393 (Bronze V)

---
+++ ProblemSolving/BOJ_2393.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class BOJ_2393 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write("  ___  ___  ___\n" +
+                "  | |__| |__| |\n" +
+                "  |           |\n" +
+                "   \\_________/\n" +
+                "    \\_______/\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |     |\n" +
+                "     |_____|\n" +
+                "  __/       \\__\n" +
+                " /             \\\n" +
+                "/_______________\\");
+        br.close();
+        bw.close();
+    }
+}
