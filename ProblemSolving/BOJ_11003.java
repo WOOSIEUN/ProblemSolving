e6e9b92d56c35af29ef20977888c37d35c3611be
Sieun Woo <ramtk6726@gmail.com>
2025-08-05 13:49:48 +0900

    BOJ 11003 (Platinum V)

--- ProblemSolving/BOJ_11003.java
+++ ProblemSolving/BOJ_11003.java
@@ -1,18 +1,9 @@
 import java.io.*;
+import java.util.ArrayDeque;
 import java.util.Deque;
-import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class BOJ_11003 {
-    static class Element {
-        int idx, num;
-
-        public Element(int idx, int num) {
-            this.idx = idx;
-            this.num = num;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -21,25 +12,23 @@ public class BOJ_11003 {
         int N = Integer.parseInt(st.nextToken());
         int L = Integer.parseInt(st.nextToken());
 
-        Deque<Element> deque = new LinkedList<>();
-        StringBuilder sb = new StringBuilder();
+        Deque<int[]> deque = new ArrayDeque<>();
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(st.nextToken());
-            while (!deque.isEmpty() && deque.peekLast().num > num) {
-                deque.removeLast();
+            while (!deque.isEmpty() && deque.peekLast()[1] > num) {
+                deque.pollLast();
             }
-            deque.addLast(new Element(i, num));
+            deque.addLast(new int[] {i, num});
 
-            if (deque.peekFirst().idx < i - L + 1) {
-                deque.removeFirst();
+            if (deque.peek()[0] < i - L + 1) {
+                deque.poll();
             }
 
-            sb.append(deque.peekFirst().num).append(" ");
+            bw.write(deque.peek()[1] + " ");
         }
 
-        bw.write(sb.toString().trim());
-        br.close();
+        bw.flush();
         bw.close();
     }
 }
