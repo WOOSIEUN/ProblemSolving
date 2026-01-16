adbb7d2a8b4e15d49a9d055c2848005f5763c90d
Sieun Woo <ramtk6726@gmail.com>
2026-01-16 13:36:39 +0900

    BOJ 16680 (Platinum V)

---
+++ BOJ_16680.java
@@ -0,0 +1,19 @@
+import java.io.*;
+
+public class BOJ_16680 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int t = 0; t < T; t++) {
+            long n = Long.parseLong(br.readLine());
+            sb.append(n * 999_999_999).append("\n");
+        }
+
+        bw.write(sb.toString());
+        br.close();
+        bw.close();
+    }
+}
