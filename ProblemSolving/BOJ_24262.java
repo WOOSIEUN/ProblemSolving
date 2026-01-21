7e8b5d81eb9611ee4d03b8741a2f1aa8a63f9d41
Sieun Woo <ramtk6726@gmail.com>
2026-01-21 13:33:01 +0900

    BOJ 24262 (Bronze V)

---
+++ ProblemSolving/BOJ_24262.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class BOJ_24262 {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        bw.write(String.format("%d %d", 1, 0));
+        bw.close();
+    }
+}
